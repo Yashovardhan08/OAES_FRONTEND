70aad8b5f21781b6f5324588264e89c8c34ed197
Tarun Reddy <tarunreddy@Taruns-MacBook-Pro.local>
2023-04-05 15:21:47 +0530

    Did changes to add and modify

--- src/Components/Pages/AddPage.js
+++ src/Components/Pages/AddPage.js
@@ -1,11 +1,130 @@
+import { useState } from "react";
+import { TextField } from "@mui/material";
+
 const AddPage = (props) => {
+  const [qid, questionID] = useState("");
+  const [question, setQuestion] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
+  const [option3, setOption3] = useState("");
+  const [option4, setOption4] = useState("");
+  const [correctOption, setOption] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The qid you entered was: ${qid}`);
+
+    event.preventDefault();
+    alert(`The question you entered was: ${question}`);
+
+    event.preventDefault();
+    alert(`The question you entered was: ${option1}`);
+
+    event.preventDefault();
+    alert(`The question you entered was: ${option2}`);
+
+    event.preventDefault();
+    alert(`The question you entered was: ${option3}`);
+
+    event.preventDefault();
+    alert(`The question you entered was: ${option4}`);
+
+    event.preventDefault();
+    alert(`The question you entered was: ${correctOption}`);
+  };
+
   return (
     <div className="pages">
-      <p>
-        ADD PAGE HERE !!
-      </p>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={qid}
+          label="Question ID required"
+          variant="outlined"
+          style={{
+            margin: "10vh 10vw 0px 0vw",
+            width: "30vw",
+            flex: "1",
+          }}
+          required
+          onChange={(e) => questionID(e.target.value)}
+        />
+        <TextField
+          value={question}
+          label="Question required"
+          variant="outlined"
+          style={{
+            margin: "5vh 10vw 0px 0vw",
+            width: "30vw",
+            flex: "1",
+          }}
+          required
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <TextField
+          value={option1}
+          label="Option 1 required"
+          variant="outlined"
+          style={{
+            margin: "5vh 10vw 0px 0vw",
+            width: "30vw",
+            flex: "1",
+          }}
+          required
+          onChange={(e) => setOption1(e.target.value)}
+        />
+        <TextField
+          value={option2}
+          label="Option 2 required"
+          variant="outlined"
+          style={{
+            margin: "5vh 10vw 0px 0vw",
+            width: "30vw",
+            flex: "1",
+          }}
+          required
+          onChange={(e) => setOption2(e.target.value)}
+        />
+        <TextField
+          value={option3}
+          label="Option 3 required"
+          variant="outlined"
+          style={{
+            margin: "5vh 10vw 0px 0vw",
+            width: "30vw",
+            flex: "1",
+          }}
+          required
+          onChange={(e) => setOption3(e.target.value)}
+        />
+        <TextField
+          value={option4}
+          label="Option 4 required"
+          variant="outlined"
+          style={{
+            margin: "5vh 10vw 0px 0vw",
+            width: "30vw",
+            flex: "1",
+          }}
+          required
+          onChange={(e) => setOption4(e.target.value)}
+        />
+        <TextField
+          value={correctOption}
+          label="Correct Option required"
+          variant="outlined"
+          style={{
+            margin: "5vh 10vw 0px 0vw",
+            width: "30vw",
+            flex: "1",
+          }}
+          required
+          onChange={(e) => setOption(e.target.value)}
+        />
+        <br></br>
+        <input type="submit" />
+      </form>
     </div>
   );
-}
+};
 
-export default AddPage
+export default AddPage;
--- src/Components/SideBar/NavigationItems.js
+++ src/Components/SideBar/NavigationItems.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from "react";
 import { SideBarData } from "./SidebarData";
 import "../../App.css";
 import { Button } from "@mui/material";
-import { useNavigate } from 'react-router';
-
+import { useNavigate } from "react-router";
 
 const NavigationItems = (props) => {
   const navigate = useNavigate();
 
-  return (<>
-    {SideBarData.map((val,key)=>{
-        return <Button key={key} startIcon={val.icon} 
-          style={{
-            color: "#F8F4E3",
-            width: "inherit",
-            paddingTop: "15px",
-            paddingBottom: "15px"
-          }}
-            onClick={(e)=>{navigate(val.link);}}
+  return (
+    <>
+      {SideBarData.map((val, key) => {
+        return (
+          <Button
+            key={key}
+            startIcon={val.icon}
+            style={{
+              color: "#F8F4E3",
+              width: "inherit",
+              paddingTop: "15px",
+              paddingBottom: "15px",
+            }}
+            onClick={(e) => {
+              navigate(val.link);
+            }}
           >
             {val.title}
-          </Button>;
+          </Button>
+        );
       })}
-  </>
+    </>
   );
-}
+};
 
 export default NavigationItems;
