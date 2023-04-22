985b30e04e3b270cdaea61e9d1bccafa3cb054d0
Tarun Reddy <tarunreddy@Taruns-MacBook-Pro.local>
2023-04-22 17:22:00 +0530

    Added register

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,87 +1,104 @@
 import { useNavigate } from "react-router";
 import { UserContext } from "./../App";
-import { useContext,useState } from "react";
+import { useContext, useState } from "react";
 import { Button, TextField } from "@mui/material";
+import { registerDB } from "./connections";
 
-const Register = ({setRegister}) => {
+const Register = ({ setRegister }) => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
   const value = useContext(UserContext);
 
-  const register = (e) => {
-    if(username==="")return;
-    if(password1!==password2){
-        setPassword1("");
-        setPassword2("");
-        return;
+  const register = async (e) => {
+    if (username === "") return;
+    if (password1 !== password2) {
+      setPassword1("");
+      setPassword2("");
+      return;
     }
     // CALL DATABASE
-    value[1](username);
-    navigate("/allQuestions");
-  }
-  
-  return <div>
-    <TextField
-        value={username} 
-        label="Username required" 
-        variant="outlined" 
+    const ret = await registerDB(username, password1);
+    console.log("Return in login :" + ret);
+    if (ret) {
+      value[1](username);
+      navigate("/allQuestions");
+    }
+    setUsername("");
+    setPassword1("");
+    setPassword2("");
+  };
+
+  return (
+    <div>
+      <TextField
+        value={username}
+        label="Username required"
+        variant="outlined"
         style={{
-            margin:"40vh 35vw 0px 35vw",
-            width:"30vw",
-            flex:"1"
+          margin: "40vh 35vw 0px 35vw",
+          width: "30vw",
+          flex: "1",
         }}
-        required 
+        required
         onChange={(e) => setUsername(e.target.value)}
-    />
-    <TextField
-        value={password1} 
-        label="Password required" 
-        variant="outlined" 
+      />
+      <TextField
+        value={password1}
+        label="Password required"
+        variant="outlined"
         style={{
-            margin:"2vh 35vw 0px 35vw",
-            width:"30vw",
-            flex:"1"
+          margin: "2vh 35vw 0px 35vw",
+          width: "30vw",
+          flex: "1",
         }}
-        required 
+        required
         type="password"
         onChange={(e) => setPassword1(e.target.value)}
-    />
-    <TextField
-        value={password2} 
-        label="Type Password Again" 
-        variant="outlined" 
+      />
+      <TextField
+        value={password2}
+        label="Type Password Again"
+        variant="outlined"
         style={{
-            margin:"2vh 35vw 0px 35vw",
-            width:"30vw",
-            flex:"1"
+          margin: "2vh 35vw 0px 35vw",
+          width: "30vw",
+          flex: "1",
         }}
-        required 
+        required
         type="password"
         onChange={(e) => setPassword2(e.target.value)}
-    />
+      />
 
-    <div style={{display:"flex",flexDirection:"row",margin:"2vh 35vw 0vh 35vw"}}>
-        <Button 
-            style={{
-                flex:"1",
-                color:"black"
-            }}
-            onClick={(e)=> setRegister(false)}>
-            LOGIN
-        </Button>
-        <Button 
+      <div
         style={{
-            flex:"1",
-            color:"black"
+          display: "flex",
+          flexDirection: "row",
+          margin: "2vh 35vw 0vh 35vw",
         }}
-        onClick={register}>
-        REGISTER
-    </Button>
+      >
+        <Button
+          style={{
+            flex: "1",
+            color: "black",
+          }}
+          onClick={(e) => setRegister(false)}
+        >
+          LOGIN
+        </Button>
+        <Button
+          style={{
+            flex: "1",
+            color: "black",
+          }}
+          onClick={register}
+        >
+          REGISTER
+        </Button>
+      </div>
     </div>
-    
-  </div>;
-}
+  );
+};
 
-export default Register
+export default Register;
--- src/Components/connections.js
+++ src/Components/connections.js
@@ -21,4 +21,24 @@ export const LoginDB = async (username, password) => {
   //   console.log(" RET FROM LOGIN :", ret);
   return ret;
 };
-export const register = async (username, password) => {};
+
+export const registerDB = async (username, password) => {
+  var ret = false;
+  const result = await fetch("http://localhost:8079/user/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username: username, password: password }),
+    //   username,
+  })
+    .then(async (res) => {
+      await res.json().then((v) => {
+        console.log(" v in res.json: ", v);
+        ret = v;
+      });
+    })
+    .catch((err) => {
+      ret = false;
+    });
+};
